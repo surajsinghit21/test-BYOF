9bdc2ca5212379b6d9982b02038bc63874129b77
surajsinghit21 <surajsinghit21@gmail.com>
2023-06-22 10:53:51 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`api/HttpTrigger1`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
+}
+
+export default App;
